37ada08317a97e0496daea8978e2d381c84c2d5d
Noe <noemk2@gmail.com>
2023-05-20 19:07:37 +0000

    end1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    App::new().add_system(hello_world).run()
+}
+
+pub fn hello_world() {
+    println!("Hello world")
 }
